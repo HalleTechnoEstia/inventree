0c075635f1b8a9abb9fc9a6a3d7b396dfcfac81c
HalleTechnoEstia <halle.technologique@estia.fr>
2025-05-28 17:14:05 +0200

    fixing scripts

---
+++ download.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ssh "root@192.168.88.189" << EOF
+ls
+EOF
+cd /home/user/inventree/
+mkdir backup
+scp root@192.168.88.189:/home/user/inventree/backup/latest.tar /home/user/inventree/backup/
+cd backup
+tar -xvf latest.tar
+docker cp /home/user/inventree/backup/media/ inventree-server:/home/inventree/data/
+docker cp /home/user/inventree/backup/data.json inventree-server:/home/inventree/data/
+docker exec -it inventree-server invoke import-records -c -f /home/inventree/data/data.json
